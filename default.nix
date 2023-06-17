46d4f8ed1060e1c186f9ca237e5d0f4d35b06977
pcs3rd <18014111+pcs3rd@users.noreply.github.com>
2023-06-17 08:48:07 -0400

    Gnome: move into subfolder

--- default.nix
+++ default.nix
@@ -1,8 +1,5 @@
 #     nix-build -A mypackage
 {pkgs ? import <nixpkgs> {}}: {
-  git-credential-manager = pkgs.callPackage ./pkgs/git-credential-manager {};
-  github-linguist = pkgs.callPackage ./pkgs/github-linguist {};
-  pkcs11-provider = pkgs.callPackage ./pkgs/pkcs11-provider.nix {};
-  sea-orm-cli = pkgs.callPackage ./pkgs/sea-orm-cli {};
+  wallpaper-lakeside = pkgs.callPackage ./packages/wallpapers/gnome/lakeside {};
 }
 
